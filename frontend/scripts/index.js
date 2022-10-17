46269b3335eab9a4f5034a04d036e6eb40cf54fc
Cole Foster <Cfoster5841@conestogac.on.ca>
2022-10-17 17:47:19 +0000

    GET from classlist, data shown in alert

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+
+function getClasslist(){
+    const Url = 'http://localhost:27501/students';
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", Url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    alert( xmlHttp.responseText);
+}
