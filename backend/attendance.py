4a16974743b0a5f16f29c37e676de44210ec149f
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2022-10-18 06:49:34 +0000

    Completed createAttendance and 1 UT 1 UT on json functionality of Attendance object

--- backend/attendance.py
+++ backend/attendance.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from typing import List
+from pydantic import BaseModel
 
 
-@dataclass
-class StudentAttendanceItem:
+class StudentAttendanceItem(BaseModel):
     studentID: str # Retrieved from classlist module
     isPresent: bool
 
-@dataclass
-class Attendance:
+
+class Attendance(BaseModel):
     id: str # Retrieve calendar
     records: List[StudentAttendanceItem]
