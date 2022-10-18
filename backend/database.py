39f8f8a3b6cb3b7cd15f26097fb0ad06c83f75b3
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2022-10-18 06:49:34 +0000

    Completed createAttendance and 1 UT 1 UT on json functionality of Attendance object

--- backend/database.py
+++ backend/database.py
@@ -5,8 +5,14 @@ class DatabaseInterface:
     """
     Provides functionality for creating and updating 
     """
+    
 
     def createAttendance(self, attendanceObject: Attendance) -> None:
+        #save the object locally 
+        file = open('backend/database/data.dat','a+')
+        if file.tell()!=0:  #this is just to make sure that unless we're starting at 0 we should create a new line to read the json
+            file.write("\n")
+        file.write(attendanceObject.json())
         pass
     
     def updateAttendance(self, attendanceObject: Attendance) -> None:
