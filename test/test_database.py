3445e382887b0ddcfd80846634bd50a753b35624
Emil Harvey <ehharvey3704@conestogac.ca>
2022-10-18 15:01:37 +0000

    Fixed unit test

--- test/test_database.py
+++ test/test_database.py
@@ -1,5 +1,5 @@
 from backend.attendance import Attendance, StudentAttendanceItem
-
+import json
 
 def test_attendance_json():
     """This is to test if the .json() functionality of Attendance object works as expected"""
@@ -9,10 +9,22 @@ def test_attendance_json():
     student_id2 = StudentAttendanceItem(studentID = "456", isPresent = True)
     my_attendance = Attendance(id="1", records = [student_id1, student_id2])
 
-    _expected = "{\"id\": \"1\", \"records\": [{\"studentID\": \"123\", \"isPresent\": false}, {\"studentID\": \"456\", \"isPresent\": true}]}"
+    _expected = json.dumps({
+        "id": "1",
+        "records": [
+            {
+                "studentID": "123",
+                "isPresent": False
+            },
+            {
+                "studentID": "456",
+                "isPresent": True
+            }
+        ]
+    })
 
     #ACT
     _actual = my_attendance.json()
+    
     #ASSERT
-
-    assert _expected != _actual , "Here is the expected json:\n" + _expected + "\nHere is the actual json:\n" + _actual
+    assert _expected == _actual
