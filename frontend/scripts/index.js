b043e0c63e475be53ee3a0fcdd424681fa37a776
Emil Harvey <ehharvey3704@conestogac.ca>
2022-10-21 01:00:59 +0000

    Basic Deployment

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,7 +1,22 @@
-console.log("Hello World")
-
 function getClasslist(){
+    const Url = 'http://192.168.2.104:27501/students';
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", Url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    alert( xmlHttp.responseText);
+}
+
+function getCalendarEvent(){
     const Url = 'http://localhost:27501/students';
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", Url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    alert( xmlHttp.responseText);
+}
+
+function getBackend(){
+    // 192.168.2.102
+    const Url = 'http://192.168.46.9:5000';
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", Url, false ); // false for synchronous request
     xmlHttp.send( null );
