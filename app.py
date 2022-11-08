136f24d83c42c98a6f537874da738a3241c4314c
ehharvey <eharvey3704@conestogac.on.ca>
2022-11-08 23:55:28 +0000

    Initial

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask # For web server
-from flask_cors import CORS, cross_origin
+from flask import Flask, request # For web server
+from backend.attendance import Attendance
+from backend.database import Database
+from pydantic import ValidationError
+from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 CORS(app)
+
+database: Database = Database()
 
 
 @app.route("/")
@@ -10,3 +15,18 @@ def hello_world():
     """Basic Hello World"""
 
     return "Attendance Backend says Hello World!"
+
+
+@app.route("/api/attendance", methods=["POST"])
+def postAttendance():
+    try:
+        attendance = Attendance(**request.json) # request.json raises 400 if no json body
+
+        database.createAttendance(attendance)
+
+        return {
+            "id": attendance.id
+        }, 201 # Return a tuple of body, return code
+
+    except ValidationError as e:
+        return e.json(), 400 # Return a tuple of body, return code
