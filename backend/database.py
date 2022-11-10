9caacf36bf5bedfc430a929986d92947aa8683f5
ehharvey <eharvey3704@conestogac.on.ca>
2022-11-10 15:40:20 +0000

    database API rewrite

--- backend/database.py
+++ backend/database.py
@@ -1,71 +1,84 @@
-from os import mkdir
 import json
 import os
+from os import mkdir
 from pathlib import Path
 from typing import List
+
 from .attendance import Attendance
 
-class DatabaseInterface:
-    """
-    Interface requirements for creating and updating 
-    """
 
-    def createAttendance(self, attendanceObject: Attendance, file_opener = open):
-        pass
-    
-    def updateAttendance(self, attendanceObject: Attendance) -> None:
-        pass
+class AttendanceAlreadyExists(Exception):
+    """Exception when Attendance item already exists"""
+    pass
 
-    def getAttendanceIDs(self) -> List[str]:
-        pass
 
-    def getAttendance(self, id: str) -> Attendance:
-        pass
-
+class AttendanceDoesNotExist(Exception):
+    """Exception when Attendance item does not exist (but it should)"""
 
 # We implement interfaces here. Avoid having direct implementations in classes marked ...Interfcace
 # (though we can implement them as stubs if needed)
-class Database(DatabaseInterface):
-    ATTENDANCE_DATABASE_PATH = Path("./backend_data/")
 
-    # We can mock dependencies easily in python by adding extra parameters for dependencies
-    # Note that we supply the function itself, not the result of a function (e.g., a function call)
-    def __init__(self, mkdir=mkdir):
-        if not self.ATTENDANCE_DATABASE_PATH.is_dir():
-            mkdir(self.ATTENDANCE_DATABASE_PATH)
 
-    def createAttendance(self, attendanceObject: Attendance, open = open):
-        full_path_name = self.ATTENDANCE_DATABASE_PATH / (attendanceObject.id + ".json")
+class Database:
+    """A Database to store Attendances"""
+
+    attendance_database_folder: Path
+
+    def __init__(
+        self,
+        database_folder: Path = Path("./backend_data/"),
+    ):
+        # Set dependencies here
+        self.attendance_database_folder = database_folder
+
+        # If attendance path is not a dir, make it one
+        if not self.attendance_database_folder.is_dir():
+            self.attendance_database_folder.mkdir()
+
+    def create_attendance(self, attendance: Attendance):
+        """
+        Creates a new attendance item. Raises AttendanceAlreadyExists if the item already exists
+
+        To update an already existing attendance item, use update_attendance
+        """
+
+        full_path_name = self.attendance_database_folder / \
+            (attendance.id + ".json")
 
         # When opening files, use this approach [with open(...)]
         # This will automatically close the file at the end of the scope
-        if (os.path.isfile(full_path_name)):
-            return # in future we should send response code that shows that we could not process this request. 
+        if (full_path_name.is_file()):
+            raise AttendanceAlreadyExists
 
+        with full_path_name.open("w") as f:
+            f.write(attendance.json())
 
-        with open(full_path_name, "w") as f:
-            f.write(attendanceObject.json())
+    def update_attendance(self, attendance: Attendance) -> None:
+        """
+        Updates an *existing* Attendance item with a new one
+        """
 
-    def updateAttendance(self, attendanceObject: Attendance) -> None:
-        full_path_name = self.ATTENDANCE_DATABASE_PATH / (attendanceObject.id + ".json")
+        full_path_name = self.attendance_database_folder / \
+            (attendance.id + ".json")
 
-        if not (os.path.isfile(full_path_name)):
-            return # in future we should send response code that shows that we could not process this request. 
-        
+        if not (full_path_name.is_file()):
+            raise AttendanceDoesNotExist
+
         # if it doesn't exist I can create the file and store it
-        with open(full_path_name, "w") as f:
-            f.write(attendanceObject.json())
+        with full_path_name.open("w") as f:
+            f.write(attendance.json())
 
-    def getAttendance(self, id: str) -> Attendance:
+    def get_attendance(self, id: str) -> Attendance:
+        """Retrieves an existing Attendance item"""
 
-        full_path_name = self.ATTENDANCE_DATABASE_PATH / (id + ".json")
-        if not (os.path.isfile(full_path_name)):
-            return # in future we should send response code that shows that we could not process this request. 
-        
+        full_path_name = self.attendance_database_folder / (id + ".json")
+        if not (full_path_name.is_file()):
+            raise AttendanceDoesNotExist
+
         # if it doesn't exist I can create the file and store it
-         # if it doesn't exist I can create the file and store it
-        with open(full_path_name, "r") as f:
+        # if it doesn't exist I can create the file and store it
+        with full_path_name.open("r") as f:
             myDataString = f.read()
             data = json.loads(myDataString)
-            attendance = Attendance (id = data["id"], records = data["records"])
+            attendance = Attendance(id=data["id"], records=data["records"])
             return attendance
