ce563da5573bf1c386708d8a14b3b2d4da61c663
Justin200104 <Jlangevin@conestogac.on.ca>
2022-11-10 17:18:40 +0000

    added a ROUTE to get attendance info

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask # For web server
+from flask import Flask  # For web server
 from flask_cors import CORS, cross_origin
+from backend.database import Database
+
 app = Flask(__name__)
 cors = CORS(app)
 CORS(app)
+
+db = Database()
 
 
 @app.route("/")
@@ -10,3 +14,11 @@ def hello_world():
     """Basic Hello World"""
 
     return "Attendance Backend says Hello World!"
+
+
+@app.route("/api/attendance/<int:attendance_id>")
+def attendance(attendance_id):
+
+    val = db.get_attendance(attendance_id)
+
+    return val
