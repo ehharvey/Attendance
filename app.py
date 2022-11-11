6632f8fe47f4a634fbbcc2b2097bf640512ce9f0
ehharvey <eharvey3704@conestogac.on.ca>
2022-11-11 04:42:27 +0000

    * Minor rewrite

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from typing import Dict
 from flask import Flask  # For web server
 from flask import request, send_from_directory, send_file
 from flask_cors import CORS, cross_origin
 from backend.database import Database
 import json
 
+from pydantic import BaseModel
 from backend.database import AttendanceAlreadyExists
 from backend.attendance import Attendance
 import requests
@@ -18,22 +20,22 @@ db = Database()
 SERVICES_JSON = Path("./services.json")
 
 
-def get_services() -> dict:
+def get_services() -> Dict[str, dict]:
     with SERVICES_JSON.open("r") as f:
         return json.load(f)
 
 
-SERVICES = get_services()
+class Service(BaseModel):
+    ip: str
+    port: str
 
 
-def get_services_url(theService: str, theRoute: str) -> str:
-    return str(
-        SERVICES[theService]["ip"]
-        + ":"
-        + str(SERVICES[theService]["port"])
-        + "/"
-        + theRoute
-    )
+SERVICES = {key: Service(**value) for key, value in get_services().items()}
+
+CALENDAR_SERVICE = SERVICES.get("calendar")
+CLASSLIST_SERVICE = SERVICES.get("classlist")
+
+SERVICE_TIMEOUT = 3  # seconds before we quit trying to communicate with others
 
 
 @app.route("/")
@@ -78,32 +80,26 @@ def attendance(attendance_id):
         return "Successfully added attendance item"
 
 
-@app.route("/api/attendance/getClasslist", methods=["GET"])
+@app.route("/api/classlist", methods=["GET"])
 def getClasslist():
-    myClasslistURL = get_services_url("classlist", "api/attendance/returnClasslist")
-    
-    response = requests.get(myClasslistURL)
-    response.encoding = "utf-8"  # Optional: requests infers this internally
 
-    return {
-        "response_status_code": response.status_code,
-        "response": response.json(),
-        "myClasslistURL": myClasslistURL,
-    }, response.status_code
+    try:
+        response = requests.get(
+            f"http://{CLASSLIST_SERVICE.ip}:{CLASSLIST_SERVICE.port}/students",
+            timeout=3,
+        )
+    except Exception as e:
+        return str(e), 500
 
 
-@app.route("/api/attendance/getCalendar", methods=["GET"])
+@app.route("/api/calendar", methods=["GET"])
 def getCalendar():
-    myCalendarURL = get_services_url("calendar", "event")
 
-    response = requests.get(myCalendarURL)
-    response.encoding = "utf-8"  # Optional: requests infers this internally
-
-    # if response.status_code == 200:
-    # return "Successfully found the classlist", 200
-
-    return {
-        "response_status_code": response.status_code,
-        "response": response.json(),
-        "myClasslistURL": myCalendarURL,
-    }, response.status_code
+    try:
+        response = requests.get(
+            f"http://{CALENDAR_SERVICE.ip}:{CALENDAR_SERVICE.port}/event", timeout=3
+        )
+        # if response.status_code == 200:
+        # return "Successfully found the classlist", 200
+    except Exception as e:
+        return str(e), 500
