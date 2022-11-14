a1caa52a6a2389435a57dc4d8610165e97974e1b
Coel Foster <cole@thefostersonline.com>
2022-11-14 19:37:11 +0000

    Fixed jest exports issue, now trying to mock xml

--- test/js_tests/frontend_functions.test.js
+++ test/js_tests/frontend_functions.test.js
@@ -1,14 +1,32 @@
-const functionA = require('../../frontend/scripts/index');
+const { logConsole, addAttendance } = require('../../frontend/scripts/index');
+global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 test('Tests function consoleLog - empty', () => {
     const d = Date();
-    expect(functionA()).toBe("[Page Load]" + " " + d);
+    expect(logConsole()).toBe("[Page Load]" + " " + d);
 });
 
-// test('Tests function consoleLog - empty', () => {
-//     const d = Date();
-//     expect(logConsole("message123")).toBe("[message123] " + d);
-// });
+test('Tests function consoleLog - with message', () => {
+    const d = Date();
+    expect(logConsole("TestMessage 123")).toBe("[TestMessage 123] " + d);
+});
+
+test('Sample test which accesses addAttendance', () => {
+    const f = jest.spyOn(XMLHttpRequest, 'open');
+    const testID = 55512;
+    const attendance_json = {   //placeholder attendance data
+        "id": testID,
+        "records": {
+            "studentID": "ABC",
+            "isPresent": true
+        }
+    }
+    const Url = '/api/attendance/' + testID; //localhost ip, change for class
+
+    addAttendance(testID);
+
+    expect(f).toHaveBeenCalledWith("POST", Url, false);
+});
 
 // test('Tests function getNextEvent', () => {
 //     const Url = 'http://192.168.2.101:27501/nextevent';
