99af4f72c89cbdd16c998d36f705ecc43ed6d565
colefoster <Cfoster5841@conestogac.on.ca>
2022-11-22 20:44:54 +0000

    Issue with getClasslist()

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,31 +1,43 @@
+// This function corrects route
+// requests to account for proxies
+// (e.g., when running in the web VM)
+function getRoute(route, route_prepend = window.location.pathname) {
+    if (route.startsWith("/")) {
+        return route_prepend + route.substring(1);
+    }
+    else {
+        return route_prepend + route;
+    }
+}
+
 function getClasslist() {
     logConsole("Getting Class List");
-    const Url = '/api/classlist';
+    const Url = getRoute('api/classlist');
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", Url, false); // false for synchronous request
     xmlHttp.send(null);
     alert(xmlHttp.responseText);
-    return xmlHttp;
+    return xmlHttp.responseText;
 }
 
 function getCalendarEvent() {
     logConsole("Getting Calendar Event");
-    const Url = '/api/calendar';
+    const Url = getRoute('/api/calendar');
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", Url, false); // false for synchronous request
     xmlHttp.send(null);
     alert(xmlHttp.responseText);
-    return xmlHttp;
+    return xmlHttp.responseText;
 }
 
 function getSummary() {
     //const Url = 'http://192.168.2.103:5000/api/attendance';//swap IP for class
-    const Url = '/api/attendance';
+    const Url = getRoute('/api/attendance');
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", Url, false); // false for synchronous request
     xmlHttp.send(null);
     alert(xmlHttp.responseText);
-    return xmlHttp;
+    return xmlHttp.responseText;
 }
 
 function addAttendance(attendance_ID) {
@@ -38,7 +50,7 @@ function addAttendance(attendance_ID) {
         }
     }
     //const Url = 'http://192.168.2.103:5000/api/attendance/' + attendance_json.id;
-    const Url = '/api/attendance/' + attendance_json.id; //localhost ip, change for class
+    const Url = getRoute('/api/attendance/' + attendance_json.id); //localhost ip, change for class
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", Url, false); // false for synchronous request
 
@@ -47,12 +59,10 @@ function addAttendance(attendance_ID) {
     xmlHttp.setRequestHeader("Content-Type", "application/json");
     xmlHttp.send(JSON.stringify(attendance_json));
     alert(xmlHttp.responseText);
-    return xmlHttp.responseText;
 }
 
 function fillAttendanceDropdown() {
     const dropDown = document.getElementById("select-5c86");
-    const options = dropDown.getElementsByTagName("option");
     const attendance_json = JSON.parse(getSummary());
     logConsole(attendance_json);
     for (let i = 0; i < attendance_json.ids.length; i++) {
@@ -62,6 +72,11 @@ function fillAttendanceDropdown() {
     }
 }
 
+function fillStudentList() {
+    const studentTable = document.getElementById("studentList");
+    //const students = JSON.parse(getClasslist());
+    logConsole(getClasslist());
+}
 /*------------------------------------------------------------------------------------------
 * Function	        :	logConsole()
 * Description	    :	This Function is used to log request and responses to the console.			
@@ -80,7 +95,8 @@ function logConsole(loggingValue) {
         console.log("[Page Load]" + " " + d);
         return ("[Page Load]" + " " + d);
     }
-
 }
 
-
+module.exports = {
+    logConsole, addAttendance, getRoute
+};
